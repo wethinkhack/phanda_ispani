bfd95b67f9e4ec5ca5f69651f77c9338f0de48ad
Lloyd <llzwenyjhb024@student.wethinkcode.co.za>
2024-11-14 00:29:20 +0200

    Lloyd at work

--- app/src/main/java/com/example/phandaispani/MainActivity.kt
+++ app/src/main/java/com/example/phandaispani/MainActivity.kt
@@ -16,32 +16,11 @@ import com.example.phandaispani.ui.theme.PhandaIspaniTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            PhandaIspaniTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhandaIspaniTheme {
-        Greeting("Android")
     }
 }
