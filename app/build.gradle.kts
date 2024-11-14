7b59751e7bfb45ae6086e0f8e096357a6e7a804b
Lloyd <llzwenyjhb024@student.wethinkcode.co.za>
2024-11-14 13:04:59 +0200

    work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.phandaispani"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.phandaispani"
